a8e802558b6a64c8dd6db573e4f00dae8d3a3e3d
hyulee <lhuy100000@gmail.com>
2024-04-18 16:43:40 +0700

    feat/tec-26 User can update Account Infomation

--- src/main/java/com/learn/techplatform/services/User/UserService.java
+++ src/main/java/com/learn/techplatform/services/User/UserService.java
@@ -1,12 +1,14 @@
 package com.learn.techplatform.services.User;
 
+import com.learn.techplatform.controllers.models.request.EditUserRequest;
+import com.learn.techplatform.controllers.models.request.SignUpRequest;
 import com.learn.techplatform.dto_modals.UserDTO;
 import com.learn.techplatform.entities.User;
 import com.learn.techplatform.services.InterfaceBaseService;
 
 public interface UserService extends InterfaceBaseService<User, String> {
-
     UserDTO getUserByEmail(String email);
     UserDTO getAuthInfo(String id);
-
+    void editUserInfo(String id, EditUserRequest editUserRequest);
+    void deleteAccount(String id);
 }
