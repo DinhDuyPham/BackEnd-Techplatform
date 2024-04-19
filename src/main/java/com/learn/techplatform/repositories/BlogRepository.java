098939d50b2f8818f85e88691de8b3e4c8db378c
Dinh Duy Pham <painitduy@gmail.com>
2024-04-20 01:56:30 +0700

    feat/tec-36 Create a new blog

--- src/main/java/com/learn/techplatform/repositories/BlogRepository.java
+++ src/main/java/com/learn/techplatform/repositories/BlogRepository.java
@@ -1,10 +1,18 @@
 package com.learn.techplatform.repositories;
 
 import com.learn.techplatform.entities.Blog;
+import com.learn.techplatform.entities.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BlogRepository extends JpaRepository<Blog, String> {
+<<<<<<< HEAD
 
+=======
+    Course findBlogById(String id);
+    boolean existsByTitle(String title);
+>>>>>>> tec-36
 }
--- src/main/java/com/learn/techplatform/services/Blog/BlogService.java
+++ src/main/java/com/learn/techplatform/services/Blog/BlogService.java
@@ -1,7 +1,9 @@
 package com.learn.techplatform.services.Blog;
 
+import com.learn.techplatform.dto_modals.BlogDTO;
 import com.learn.techplatform.entities.Blog;
 import com.learn.techplatform.services.InterfaceBaseService;
 
 public interface BlogService extends InterfaceBaseService<Blog, String> {
+    void createBlog(BlogDTO blog, String userId);
 }
