0fb78eb54b62f5c9a556caaa9e4911f72de83b9d
hyulee <lhuy100000@gmail.com>
2024-04-20 03:17:46 +0700

    feat/tec-32 get pagination Course, can query by updated time, title,...

--- src/main/java/com/learn/techplatform/repositories/CourseRepository.java
+++ src/main/java/com/learn/techplatform/repositories/CourseRepository.java
@@ -4,7 +4,11 @@ import com.learn.techplatform.common.enums.CourseType;
 import com.learn.techplatform.common.enums.SystemStatus;
 import com.learn.techplatform.dto_modals.CourseDTO;
 import com.learn.techplatform.entities.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,4 +16,11 @@ public interface CourseRepository extends JpaRepository<Course, String> {
     Course findCourseByIdAndCourseType(String id, CourseType type);
     Course findCourseByIdAndSystemStatus(String id, SystemStatus systemStatus);
     boolean existsByTitle(String title);
+
+    @Query("""
+        SELECT c 
+        FROM Course c 
+        WHERE c.title LIKE :search_key
+    """)
+    Page<Course> getPageCourse(@Param("search_key") String searchKey, Pageable pageable);
 }
