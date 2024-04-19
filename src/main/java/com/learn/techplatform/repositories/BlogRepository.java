e23fbbe86eccc477f6d05150bce7b473ecb6f624
Dinh Duy Pham <painitduy@gmail.com>
2024-04-20 02:01:13 +0700

    feat/tec-36 Create a new blog

--- src/main/java/com/learn/techplatform/repositories/BlogRepository.java
+++ src/main/java/com/learn/techplatform/repositories/BlogRepository.java
@@ -9,10 +9,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BlogRepository extends JpaRepository<Blog, String> {
-<<<<<<< HEAD
-
-=======
     Course findBlogById(String id);
     boolean existsByTitle(String title);
->>>>>>> tec-36
 }
