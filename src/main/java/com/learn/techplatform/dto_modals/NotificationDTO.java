0a282a4a1c3b1dab823b51a82809fa9ff91ebae7
hyulee <lhuy100000@gmail.com>
2024-04-29 08:02:26 +0700

    feat/tec-48 Create Table Notification

---
+++ src/main/java/com/learn/techplatform/dto_modals/NotificationDTO.java
@@ -0,0 +1,22 @@
+package com.learn.techplatform.dto_modals;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonNaming(PropertyNamingStrategy.LowerCaseWithUnderscoresStrategy.class)
+public class NotificationDTO {
+    private String title;
+    private String content;
+    private String linked;
+    private boolean seen;
+    private String userId;
+}
